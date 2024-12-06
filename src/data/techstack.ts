ee9cd56be04a84618a1c2e75c8f5a790111e1828
kahseng-dev <kahseng.chew@protonmail.com>
2024-12-06 21:44:21 +0800

    chore: update techstack data

--- src/data/techstack.ts
+++ src/data/techstack.ts
@@ -1,13 +1,89 @@
 export type Tech = {
 	name: string,
 	category: string,
-	projects?: string[]
+	iconURL?: string
 }
 
 export const techstack: Tech[] = [
 	{
+		name: "react",
+		category: "web dev"
+	},
+	{
 		name: "svelte",
-		category: "framework",
-		projects: ["kahseng.dev"]
-	}
+		category: "web dev"
+	},
+	{
+		name: "html",
+		category: "web dev"
+	},
+	{
+		name: "css",
+		category: "web dev"
+	},
+	{
+		name: "java",
+		category: "mobile dev"
+	},
+	{
+		name: "swift",
+		category: "mobile dev"
+	},
+	{
+		name: "flutter",
+		category: "mobile dev"
+	},
+	{
+		name: "javascript",
+		category: "langauges"
+	},
+	{
+		name: "typescript",
+		category: "langauges"
+	},
+	{
+		name: "python",
+		category: "langauges"
+	},
+	{
+		name: "tailwindcss",
+		category: "web dev"
+	},
+	{
+		name: "nodejs",
+		category: "tools"
+	},
+	{
+		name: "mysql",
+		category: "database"
+	},
+	{
+		name: "supabase",
+		category: "database"
+	},
+	{
+		name: "pocketbase",
+		category: "database"
+	},
+	{
+		name: "arudino",
+		category: "internet of things"
+	},
+	{
+		name: "raspberry pi",
+		category: "internet of things"
+	},
+	{
+		name: "figma",
+		category: "tools"
+	},
+	{
+		name: "photoshop",
+		category: "tools"
+	},
+	{
+		name: "pega systems",
+		category: "web dev",
+		iconURL: "https://icons.duckduckgo.com/ip3/www.pega.com.ico"
+	},
 ] 
