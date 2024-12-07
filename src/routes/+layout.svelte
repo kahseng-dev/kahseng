2bb78e5f4028b6e5a45c84a745172d96171533c5
kahseng-dev <kahseng.chew@protonmail.com>
2024-12-07 18:34:40 +0800

    refactor: remove unnecessary ui folder

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import '../dark.theme.css';
-	import Footer from '$components/ui/Footer.svelte';
+	import Footer from '$components/Footer.svelte';
 </script>
 
 <main class="text-xs mx-auto max-w-screen-sm overflow-x-hidden px-6 py-24 md:overflow-x-visible">
