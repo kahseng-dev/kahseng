a8e0186a872aeac8d5af17a42cc9fee081306f06
kahseng-dev <kahseng.chew@protonmail.com>
2025-03-15 02:43:00 +0800

    feat: add Tooltip component

---
+++ src/lib/components/Tooltip.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    let { 
+        image,
+        tooltipText
+    } = $props();
+    
+    let isVisible = $state(false);
+    
+    function handleHover() {
+        isVisible = !isVisible;
+    }
+</script>
+
+<button 
+    onfocus={handleHover}
+    onmouseover={handleHover} 
+    onmouseout={handleHover}
+    onblur={handleHover}
+    class="border border-(--border-0) rounded p-1 relative"
+    >
+    {#if isVisible}
+        <span class="z-10 absolute bottom-11 left-0 whitespace-nowrap border border-(--border-0) text-[0.65rem] rounded bg-(--bg-0) px-1.5 py-0.5">{tooltipText}</span>
+    {/if}
+    <img
+        class="cursor-pointer size-8 p-0.5"
+        draggable="false"
+        src="{image}"
+        alt="{tooltipText}"
+    />
+</button>
