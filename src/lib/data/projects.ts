da05f72c2e3d157e3d9c0d02bbe9d2aac9ef0828
kahseng-dev <kahseng.chew@protonmail.com>
2025-03-15 01:54:29 +0800

    refactor: rename type parameters

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -8,16 +8,16 @@ export const projects: Project[] = [
 	{
 		slug: "example-1",
 		date_published: new Date(2024, 11, 6),
-		content: "placeholder"
+		content: "placeholder 1"
 	},
 	{
 		slug: "example-2",
 		date_published: new Date(2024, 11, 6),
-		content: "placeholder"
+		content: "placeholder 2"
 	},
 	{
 		slug: "example-3",
 		date_published: new Date(2024, 11, 6),
-		content: "placeholder"
+		content: "placeholder 3"
 	}
 ]
--- src/lib/data/techstacks.ts
+++ src/lib/data/techstacks.ts
@@ -1,90 +1,103 @@
 export type TechStack = {
     name: string,
-	category: string,
-	iconURL?: string
+	icon: string,
+	category: string
 }
 
 export const techstacks: TechStack[] = [
 	{
 		name: "react",
+		icon: "https://svgl.app/library/react_dark.svg",
 		category: "web dev"
 	},
 	{
 		name: "svelte",
+		icon: "https://svgl.app/library/svelte.svg",
 		category: "web dev"
 	},
 	{
 		name: "html5",
+		icon: "https://svgl.app/library/html5.svg",
 		category: "web dev"
 	},
 	{
 		name: "css",
+		icon: "https://svgl.app/library/css.svg",
 		category: "web dev"
 	},
 	{
 		name: "java",
+		icon: "https://svgl.app/library/java.svg",
 		category: "mobile dev"
 	},
 	{
 		name: "swift",
+		icon: "https://svgl.app/library/swift.svg",
 		category: "mobile dev"
 	},
 	{
 		name: "flutter",
+		icon: "https://svgl.app/library/flutter.svg",
 		category: "mobile dev"
 	},
 	{
 		name: "javascript",
-		category: "langauges"
+		icon: "https://svgl.app/library/javascript.svg",
+		category: "web dev"
 	},
 	{
 		name: "typescript",
-		category: "langauges"
+		icon: "https://svgl.app/library/typescript.svg",
+		category: "web dev"
 	},
 	{
 		name: "python",
-		category: "langauges"
+		icon: "https://svgl.app/library/python.svg",
+		category: "data analytics"
 	},
 	{
 		name: "tailwindcss",
+		icon: "https://svgl.app/library/tailwindcss.svg",
 		category: "web dev"
 	},
 	{
 		name: "nodejs",
-		category: "tools"
+		icon: "https://svgl.app/library/nodejs.svg",
+		category: "web dev"
 	},
 	{
 		name: "mysql",
+		icon: "https://svgl.app/library/mysql.svg",
 		category: "database"
 	},
 	{
 		name: "supabase",
-		category: "database"
-	},
-	{
-		name: "pocket-base",
+		icon: "https://svgl.app/library/supabase.svg",
 		category: "database"
 	},
 	{
 		name: "arudino",
-		category: "internet of things",
-		iconURL: "https://www.arduino.cc/favicon.ico"
+		icon: "https://upload.wikimedia.org/wikipedia/commons/5/5b/Arduino_Logo_Registered.svg",
+		category: "internet of things"
 	},
 	{
 		name: "raspberry_pi",
+		icon: "https://svgl.app/library/raspberry_pi.svg",
 		category: "internet of things"
 	},
 	{
 		name: "figma",
+		icon: "https://svgl.app/library/figma.svg",
 		category: "tools"
 	},
 	{
 		name: "photoshop",
+		icon: "https://svgl.app/library/photoshop.svg",
 		category: "tools"
 	},
 	{
 		name: "pega systems",
-		category: "web dev",
-		iconURL: "https://icons.duckduckgo.com/ip3/www.pega.com.ico"
+		icon: "https://icons.duckduckgo.com/ip3/www.pega.com.ico",
+		category: "web dev"
 	},
 ] 
