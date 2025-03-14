7538c8d8b138a813f9a43c767fb04a854aa92579
kahseng-dev <kahseng.chew@protonmail.com>
2025-03-14 22:00:18 +0800

    feat: add time component

---
+++ src/lib/components/Time.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let {
+        date,
+        locale = "en-GB",
+        timeformat = { day: "numeric", month: "short", year: "numeric" }
+    } = $props();
+</script>
+
+<span>
+    {date.toLocaleDateString(locale, timeformat)}
+</span>
