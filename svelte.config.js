065f73c10d29fb92dc08e94f13e1d2980dcaa38e
kahseng-dev <kahseng.chew@protonmail.com>
2025-03-14 18:59:23 +0800

    refactor: remove unused aliases

--- svelte.config.js
+++ svelte.config.js
@@ -13,10 +13,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
-			$src: './src',
-			$lib: './src/lib',
-			$data: './src/data',
-			$components: './src/lib/components'
+			$components: "./src/lib/components"
 		}
 	}
 };
