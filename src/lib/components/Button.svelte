a282c6b9c53f67fff4a2f7322c912ee9492dd7aa
kahseng-dev <kahseng.chew@protonmail.com>
2025-03-14 21:59:56 +0800

    feat: add button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    let { 
+        text = undefined, 
+        icon = undefined, 
+        color = undefined, 
+        class:className = undefined,
+        ...rest
+    } = $props();
+</script>
+
+<button class="cursor-pointer flex rounded px-2 py-2 duration-300 ease-in-out bg-black hover:bg-black/50" aria-label="{icon}" {...rest}>
+    <i class="ph ph-{icon}"></i>
+    {text}
+</button>
