401d430206610be3b621eb21ca28a3de7d525e2c
kahseng-dev <kahseng.chew@protonmail.com>
2025-03-14 21:58:00 +0800

    chore: remove unecessary stores

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,6 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
-	
-	type Theme = 'dark' | 'light';
-
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
--- src/lib/stores.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-
-const theme = writable('dark');
-
-export { theme }
