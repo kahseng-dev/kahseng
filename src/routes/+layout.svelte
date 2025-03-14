ef87af185c992518a2c1ccbd714bcce6ce3d20b8
kahseng-dev <kahseng.chew@protonmail.com>
2025-03-15 04:30:27 +0800

    chore: update slot to render

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,13 @@
 	import "../app.css";
 	import "$lib/theme/dark.theme.css";
 	import Footer from '$components/Footer.svelte';
+
+	let { children } = $props();
 </script>
 
 <div class="grid grid-cols-4 py-30 bg-(--bg-0) text-(--text-0) md:text-sm">
 	<div class="min-h-screen col-start-2 col-span-2">
-		<slot/>
+		{@render children()}
 		<Footer/>
 	</div>
 </div>
