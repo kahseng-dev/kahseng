6f49fd02c6a0ced6caa92a3eadfc11bb250fafbe
kahseng-dev <kahseng.chew@protonmail.com>
2025-03-15 02:57:52 +0800

    feat: add class prop to time component

--- src/lib/components/Time.svelte
+++ src/lib/components/Time.svelte
@@ -2,10 +2,11 @@
     let {
         date,
         locale = "en-GB",
-        timeformat = { day: "numeric", month: "short", year: "numeric" }
+        timeformat = { day: "numeric", month: "short", year: "numeric" },
+        class: className = undefined
     } = $props();
 </script>
 
-<span>
+<span class="{className}">
     {date.toLocaleDateString(locale, timeformat)}
 </span>
