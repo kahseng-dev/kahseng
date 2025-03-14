3ba9f2cc0484bd8aef24fa18aed01d30de9d55ef
kahseng-dev <kahseng.chew@protonmail.com>
2025-03-15 02:42:42 +0800

    refactor: loading of home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,100 +1,75 @@
-<script>
-	import Time from 'svelte-time';
-	import { Popover, Tooltip } from 'bits-ui';
-	import Footer from '$components/Footer.svelte';
+<script lang="ts">
+	import { techstacks } from "$lib/data/techstacks";
+	import { projects } from "$lib/data/projects";
+	import Toast from "$components/Toast.svelte";
+	import Tooltip from "$components/Tooltip.svelte";
+	import Time from "$components/Time.svelte";
 
-	let { data } = $props();
+	let uniqueTechStackCategory:string[] = [...new Set(techstacks.map((tech) => tech.category))];
+	let email:string = $state("kahseng.chew@protonmail.com");
 
-	const email = 'kahseng.chew@protonmail.com';
+	function copyToClipboard(text:string) {
+		navigator.clipboard.writeText(text);
+	}
 </script>
 
 <svelte:head>
 	<title>kahseng</title>
 </svelte:head>
 
-<main>
-	<header>
-		<container class="flex flex-col justify-between md:flex-row">
-			<h1>hello! i'm kahseng</h1>
-			<Popover.Root>
-				<Popover.Trigger
-					class="flex cursor-pointer text-tiny rounded transition-colors duration-200 hover:bg-[var(--bg-1)] md:px-1"
-					onclick={() => navigator.clipboard.writeText(email)}
-				>
-					{email}
-				</Popover.Trigger>
-				<Popover.Content class="select-none border p-1.5 text-[0.5rem]" side="top" sideOffset={4}>
-					<Popover.Arrow class="rounded-[2px] border-l border-t" />
-					copied to clipboard!
-				</Popover.Content>
-			</Popover.Root>
-		</container>
-		<p class="text-muted">mobile and full-stack web developer</p>
-	</header>
-	<section>
-		<p class="mt-4">
-			i'm a developer who works on both frontend and backend, i love focusing on user experience,
-			paying close attention to every detail, and creating beautiful and intuitive interfaces that
-			blend aesthetics with usability. <br /><br />
-			here i'll be sharing some projects that i've worked on. always striving to create experiences that
-			are not just visually appealing but also highly functional.
-		</p>
-	</section>
-	<section>
-		<h2 class="mt-4 py-2 text-muted">tech stack</h2>
-		{#each data.uniqueTechCategories as category}
-			<article class="py-2 border-t flex justify-between">
-				<p class="w-1/2">{category}</p>
-				<p class="w-1/2 flex flex-wrap gap-2">
-					{#each data.techstack as tech}
-						{#if tech.category === category}
-							<Tooltip.Root openDelay={0}>
-								<Tooltip.Trigger
-									class="p-2 rounded border transition-colors duration-200 hover:bg-[--bg-1]"
-								>
-									<img
-										class="disable-drag size-6 bg-transparent"
-										src="https://svgl.app/library/{tech.name}.svg"
-										onerror={(event) => {
-											const target = event.target;
-											if (target instanceof HTMLImageElement) {
-												target.src = tech.iconURL || 'icons/x.svg';
-											}
-										}}
-										alt={tech.name}
-									/>
-								</Tooltip.Trigger>
-								<Tooltip.Content class="select-none border p-1.5 text-[0.5rem]" sideOffset={4}>
-									<Tooltip.Arrow class="rounded-[2px] border-l border-t" />
-									{tech.name}
-								</Tooltip.Content>
-							</Tooltip.Root>
-						{/if}
+<div class="flex flex-col gap-8">
+	<div>
+		<div class="flex justify-between">
+			<span>hello! i'm kahseng</span>
+			<Toast text={email} toastText="copied to clipboard" onclick={() => copyToClipboard(email)} />
+		</div>
+		<p>mobile and full-stack web developer</p>
+	</div>
+	<p>
+		i'm a developer who works on both frontend and backend, i love focusing on user experience,
+		paying close attention to every detail, and creating beautiful and intuitive interfaces that
+		blend aesthetics with usability.<br/><br/>
+		here i'll be sharing some projects that i've worked on. always striving to create experiences that
+		are not just visually appealing but also highly functional.
+	</p>
+	<div class="flex flex-col gap-2">
+		<p>tech stack</p>
+		<hr/>
+		{#each uniqueTechStackCategory as category, index}
+			<div class="flex justify-between">
+				{category}
+				<div class="w-1/2 flex flex-wrap gap-2">
+					{#each techstacks.filter(tech => tech.category === category) as techstack}
+						<div class="flex items-center justify-center">
+							<Tooltip tooltipText={techstack.name} image={techstack.icon} />
+						</div>
 					{/each}
-				</p>
-			</article>
+				</div>
+			</div>
+			{#if index != uniqueTechStackCategory.length - 1}
+				<hr/>
+			{/if}
 		{/each}
-	</section>
-	<section>
-		<h2>
-			<a class="flex items-center mt-4 py-2 text-muted" href="/projects">
+	</div>
+	<div class="flex flex-col gap-2">
+		<a href="/projects">
+			<span class="flex items-center gap-2">
 				projects
-				<svg class="size-3 ml-1 mt-px" viewBox="0 0 24 24">
-					<path
-						class="stroke-[--text-1] stroke-2"
-						d="M3 12L21 12M21 12L12.5 3.5M21 12L12.5 20.5"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-					/>
-				</svg>
+				<i class="ph ph-arrow-right"></i>
+			</span>
+		</a>
+		<hr/>
+		{#each projects as { slug, date_published }, index}
+			<a href="/projects/{slug}" class="flex justify-between">
+				<span class="flex items-center gap-2">
+					{slug}
+					<i class="ph ph-arrow-right"></i>
+				</span>
+				<Time date={date_published}/>
 			</a>
-		</h2>
-		{#each data.projects as { slug, date_published }}
-			<article class="py-2 border-t flex justify-between">
-				<a href="/projects/{slug}">{slug}</a>
-				<Time class="text-muted" timestamp={date_published} format="MMM D, YYYY" />
-			</article>
+			{#if index != projects.length - 1}
+				<hr/>
+			{/if}
 		{/each}
-	</section>
-	<Footer />
-</main>
+	</div>
+</div>
