2d2f5f94bf6f83c53548a3519d5698fc828fd87d
kahseng-dev <kahseng.chew@protonmail.com>
2025-06-07 13:40:24 +0800

    refactor: update brand icon tooltip component

---
+++ src/lib/ui/brand-icons.svelte
@@ -0,0 +1,33 @@
+<script lang='ts'>
+    import { styles } from '$lib/constants';
+
+    let { icon, name } = $props();
+
+    let isIconHovered = $state(false);
+
+    const handleMouseEnter = () => {
+        isIconHovered = true;
+    }
+
+    const handleMouseLeave = () => {
+        isIconHovered = false;
+    }
+</script>
+
+<div class='relative'>
+    {#if isIconHovered}
+        <div class='border {styles.border} absolute pb-0.5 px-1 text-xs left-0 right-0 bottom-12 m-auto w-min whitespace-nowrap rounded bg-(--bg)'>
+            <span>{name}</span>
+        </div>
+    {/if}
+    <button
+        onmouseenter={handleMouseEnter}
+        onmouseleave={handleMouseLeave}
+        class='border {styles.border} rounded transition duration-300 cursor-pointer hover:bg-white/20'>
+        <img
+            class='size-8 m-1'
+            draggable='false'
+            src={icon}
+            alt={name} />
+    </button>
+</div>
