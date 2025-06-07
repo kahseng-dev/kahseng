d52c23b6a8bdbd63e938cde0cb09687d30b7dc75
kahseng-dev <kahseng.chew@protonmail.com>
2025-06-07 13:39:48 +0800

    refactor: update footer component

---
+++ src/lib/ui/footer.svelte
@@ -0,0 +1,52 @@
+<script lang='ts'>
+    import { styles } from '$lib/constants';
+
+    import {
+        IconBrandLinkedin,
+        IconBrandGithub,
+        IconMoon,
+        IconSun,
+    } from '@tabler/icons-svelte';
+
+    let { theme = $bindable() } = $props();
+</script>
+
+<div class='mt-12 text-xs md:text-sm'>
+    <hr class={styles.border} />
+    <div class='mt-1 flex justify-between'>
+        <div class='flex flex-col md:flex-row md:gap-8'>
+            <a
+                href='https://www.linkedin.com/in/kahseng'
+                target='_blank'
+                class='flex gap-2 items-center {styles.text_muted} transition duration-300 hover:text-white'>
+                <IconBrandLinkedin class={styles.icon} />
+                <span>linkedin</span>
+            </a>
+            <a
+                href='https://github.com/kahseng-dev'
+                target='_blank'
+                class='flex gap-2 items-center {styles.text_muted} transition duration-300 hover:text-white'>
+                <IconBrandGithub class={styles.icon} />
+                <span>github</span>
+            </a>
+        </div>
+        <div class='p-1 *:px-0.5 *:pb-0.5 flex flex-col md:flex-row gap-1 border {styles.border} rounded'>
+            <label class='rounded cursor-pointer transition duration-300 hover:bg-black/20 has-checked:bg-black/20'>
+                <input
+                    bind:group={theme}
+                    value='dark'
+                    type='radio'
+                    class='hidden' />
+                <IconMoon class={styles.icon} />
+            </label>
+            <label class='rounded cursor-pointer transition duration-300 hover:bg-black/20 has-checked:bg-black/20'>
+                <input
+                    bind:group={theme}
+                    value='light'
+                    type='radio'
+                    class='hidden' />
+                <IconSun class={styles.icon} />
+            </label>
+        </div>
+    </div>
+</div>
