d5afe3ff2e9bbe06f5ad142d243c65f8abd53651
kahseng-dev <kahseng.chew@protonmail.com>
2025-06-07 13:39:58 +0800

    refactor: update date format component

---
+++ src/lib/ui/date-format.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+    let { 
+            date,
+            locale = "en-GB",
+            format = {
+                day: "numeric",
+                month: "short",
+                year: "numeric"
+            },
+            className = '',
+        } = $props();
+
+    const formattedDate = new Date(date).toLocaleDateString(locale, format);
+</script>
+
+<span class={className}>{formattedDate}</span>
